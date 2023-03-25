0a141a3f0012a43f10596d300f23845e63fa6c3f
KalinQAQ <1026486353@qq.com>
2023-03-25 21:55:57 +0800

    注册接口

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -7,6 +7,12 @@ class UserService {
     const result = await conn.execute(statement, [name, password])
     return result
   }
+
+  async getUserByName(name) {
+    const statement = 'SELECT * FROM `user` WHERE name = ?;'
+    const [result] = await conn.execute(statement, [name])
+    return result
+  }
 }
 
 module.exports = new UserService()
